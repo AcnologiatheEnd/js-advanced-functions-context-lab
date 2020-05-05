ba2bef3982a097ddf1525cf04aac05ea544cfa1e
Winston  Chung <18.chung.winston@ctkprep.org>
2020-05-05 21:04:42 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,86 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
+function createEmployeeRecord(data) {
+  return { 
+  firstName: data[0], 
+  familyName: data[1],
+  title: data[2],
+  payPerHour: data[3],
+  timeInEvents: [],
+  timeOutEvents: [],
+  }
+}
+
+function createEmployeeRecords(data) {
+  return data.map( function(array) {
+    return createEmployeeRecord(array) 
+  })
+}
+
+function createTimeInEvent(day) {
+  let time = day.split(" ")
+  let hour = parseInt(time[1].slice(0,5))
+  let date = time[0]
+
+  let newObject = {
+    type: "TimeIn",
+    hour: hour,
+    date: date
+  }
+
+  this.timeInEvents.push(newObject)
+
+  return this
+}
+
+function createTimeOutEvent(day) {
+  let time = day.split(" ")
+  let hour = parseInt(time[1].slice(0,5))
+  let date = time[0]
+
+  let newObject = {
+    type: "TimeOut",
+    hour: hour,
+    date: date
+  }
+
+  this.timeOutEvents.push(newObject)
+
+  return this
+}
+
+function hoursWorkedOnDate(employee){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === employee
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === employee
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+function wagesEarnedOnDate(date) {
+  let wage = hoursWorkedOnDate.call(this, date) * this.payPerHour
+  return wage
+}
+
+function calculatePayroll(arrayOfEmployees) {
+  return arrayOfEmployees.reduce(function(memo, employee) {
+    return memo + allWagesFor.call(employee)
+  }, 0)
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+  return employees.find(function(employee) { return employee.firstName === firstName})
+}
+
+
+
+function allWagesFor() {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
